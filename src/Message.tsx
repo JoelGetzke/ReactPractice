f1f9b0a4e928319bb3a8f36ca801daaec3431033
Joel Getzke <JoelGetzke@users.noreply.github.com>
2024-11-11 21:01:33 -0600

    component and fragment React refresher practice

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,8 +1,8 @@
-function Message () {
-    const name = 'Joel'
+function Message() {
+    const name = 'Joel';
     if (name)
-    return <h1>Hello {name}</h1>;
-    return <h1>Hello world!</h1>
+    return <h1>Hello {name}!</h1>;
+    return <h1>Hello World!</h1>;
 }
 
 export default Message;
