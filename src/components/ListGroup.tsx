20692b935d63b5936b2b44b69298fe80e0627362
Joel Getzke <JoelGetzke@users.noreply.github.com>
2024-11-12 22:00:52 -0600

    conditional rendering practice/review

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,21 @@
-
-
 function ListGroup() {
-  return (
-    <>
-        <h1>List</h1>
-        <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
-        </ul>
-    </>
-  );
-}
+    let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+    items = [];
 
-export default ListGroup;
+
+    return (
+      <>
+        <h1>List</h1>
+        
+        {items.length === 0 && <p>No Item found</p>}
+        <ul className="list-group">
+          {items.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+  
+  export default ListGroup;
+  
