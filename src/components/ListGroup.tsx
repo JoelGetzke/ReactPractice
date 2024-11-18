b6312f081083ee2a93bc2e2cc1a46bda5f5dc6b0
Joel Getzke <JoelGetzke@users.noreply.github.com>
2024-11-17 20:45:01 -0600

    state and props review

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,21 +1,46 @@
-function ListGroup() {
-    let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-    items = [];
+import { useState } from "react";
+interface Props {
+  items: string[];
+  heading: string;
+
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({items, heading, onSelectItem} : Props) {
+  
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
 
-    return (
-      <>
-        <h1>List</h1>
-        
-        {items.length === 0 && <p>No Item found</p>}
-        <ul className="list-group">
-          {items.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      </>
-    );
-  }
-  
-  export default ListGroup;
-  
+  return (
+    <>
+      <h1>{heading}</h1>
+
+      {items.length === 0 && <p>No Item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
+
+
+
+
+
