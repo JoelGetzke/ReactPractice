68639673bc478718658fff3a1fd761c868f3b3c4
Joel Getzke <JoelGetzke@users.noreply.github.com>
2024-11-19 19:56:05 -0600

    continued react review

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/alert";
+import Button from "./components/button";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+   <Alert>
+   Hello <span>World</span>
+   </Alert>
+   <Button onClick={() => console.log('Clicked')}>Click me</Button>
     </div>
+    
   );
 }
 
